8e7eac8fdc657b1fdcb49cde70f9379d8d95409d
DPachoGit <davidpacho1513@gmail.com>
2023-07-02 13:18:36 +0200

    Commit para entrega

--- components/cartCreate.js
+++ components/cartCreate.js
@@ -1,83 +1,91 @@
 import FetchFromApi from "./fetchApi.js";
 
 class FormCreate {
-    /* El contructor guarda tags seleccionados por id y event liseners de la propia clase creada*/
-    constructor() {
+  /* El contructor guarda tags seleccionados por id y event liseners de la propia clase creada*/
+  constructor() {
     /* Creo una instancia de la clase FetchFromApi()*/
     this.newFetch = new FetchFromApi();
     /* Selecciono por id el selector y le añado un event lisener con un bind apuntando a la clase misma */
     this.brandSelector = document.getElementById("brandSelector");
-    this.brandSelector.addEventListener("change", this.handleBrandSelection.bind(this));
+    this.brandSelector.addEventListener(
+      "change",
+      this.handleBrandSelection.bind(this)
+    );
     /* Selecciono por id el selector y le añado un event lisener con un bind apuntando a la clase misma */
     this.modelSelector = document.getElementById("modelSelector");
-    this.modelSelector.addEventListener("change", this.handleModelSelection.bind(this));
+    this.modelSelector.addEventListener(
+      "change",
+      this.handleModelSelection.bind(this)
+    );
     /* Selecciono por id el tag p */
     this.emissionsResult = document.getElementById("emissionsResult");
     /* Selecciono por id el input y el tag p y le añado un event lisener con un bind apuntando a la clase misma */
     this.kmInput = document.getElementById("kmInput");
     this.kmValueInput = document.getElementById("kmValueInput");
     this.kmInput.addEventListener("input", this.handleKmInputChange.bind(this));
-    this.kmValueInput.addEventListener("input", this.handleKmValueInputChange.bind(this));
+    this.kmValueInput.addEventListener(
+      "input",
+      this.handleKmValueInputChange.bind(this)
+    );
     this.kmValue = this.kmInput.value;
   }
-    /* Añade a brandSelector los nombres que salen de hacer un fetch con el metodo getBrand */
-    async fillBrandSelector() {
+  /* Añade a brandSelector los nombres que salen de hacer un fetch con el metodo getBrand */
+  async fillBrandSelector() {
     /* Por cada marca crea una opcion para el selector brandSelector. Como nombre le pone el name y como valor la id */
     const brandList = await this.newFetch.getBrand();
     brandList.forEach((brand) => {
-    const option = document.createElement("option");
-    option.value = brand.id;
-    option.textContent = brand.name;
-    this.brandSelector.appendChild(option);
+      const option = document.createElement("option");
+      option.value = brand.id;
+      option.textContent = brand.name;
+      this.brandSelector.appendChild(option);
     });
-    }
-    /* Con el event lisener que hay en constructor cuando cambie el selector de brand */
-    async handleBrandSelection(event) {
+  }
+  /* Con el event lisener que hay en constructor cuando cambie el selector de brand */
+  async handleBrandSelection(event) {
     /* Guardamos en una variable la id "value" haciendo target a la opcion que esta siendo seleccionada */
     const selectedBrandId = event.target.value;
     /* Limpiamos el selector de modelos cada vez que seleccionemos otra marca esperamos al fetch y rellenamos la seccion con los nuevos modelosa */
-    this.clearModelSelector()
+    this.clearModelSelector();
     const modelList = await this.newFetch.getModel(selectedBrandId);
     this.fillModelSelector(modelList);
-    }
-    /* Cuando recibamos la lista de modelos por cada uno */
-    fillModelSelector(modelList) {
+  }
+  /* Cuando recibamos la lista de modelos por cada uno */
+  fillModelSelector(modelList) {
     modelList.forEach((model) => {
-    const option = document.createElement("option");
-    option.value = model.id;
-    option.textContent = `${model.name} / ${model.year}`;
-    this.modelSelector.appendChild(option);
+      const option = document.createElement("option");
+      option.value = model.id;
+      option.textContent = `${model.name} / ${model.year}`;
+      this.modelSelector.appendChild(option);
     });
-    }
-    /* Cuando selecionas un modelo se hace un fetch para sacar una estimacion de co2 y pegarla en el tag p */
-    handleModelSelection(event) {
-      const selectedModelId = event.target.value;
-  
-      this.newFetch
-        .getEstimates(selectedModelId, this.kmValue)
-        .then((emissions) => {
-          this.emissionsResult.textContent = emissions + "kg";
-        })
-        .catch((err) => {
-          console.error(err);
-        });
-    }
-    /* Mientras que en el modelSelector tengamos opciones elimina opciones */
-    clearModelSelector() {
-      while (this.modelSelector.firstChild) {
-      this.modelSelector.removeChild(this.modelSelector.firstChild);
-      }
-      }
-    /* actualiza el valor de kmInput cuando el event lisener del contructor recive un input luego actualiza el valor de kmValueInput*/
-    handleKmInputChange() {
-      this.kmValue = this.kmInput.value; // Actualiza el valor de kmValue al modificar kmInput
-      this.kmValueInput.value = this.kmValue; // Actualiza el valor de kmValueInput
-    }  
-    /* actualiza el valor de kmValueInput cuando el event lisener del contructor recive un input luego actualiza el valor de kmInput*/
-    handleKmValueInputChange() {
-      this.kmValue = this.kmValueInput.value; // Actualiza el valor de kmValue al modificar kmValueInput
-      this.kmInput.value = this.kmValue; // Actualiza el valor de kmInput
-    }
-}
+  }
+  /* Cuando selecionas un modelo se hace un fetch para sacar una estimacion de co2 y pegarla en el tag p */
+  handleModelSelection(event) {
+    const selectedModelId = event.target.value;
 
+    this.newFetch
+      .getEstimates(selectedModelId, this.kmValue)
+      .then((emissions) => {
+        this.emissionsResult.textContent = emissions + "kg";
+      })
+      .catch((err) => {
+        console.error(err);
+      });
+  }
+  /* Mientras que en el modelSelector tengamos opciones elimina opciones */
+  clearModelSelector() {
+    while (this.modelSelector.firstChild) {
+      this.modelSelector.removeChild(this.modelSelector.firstChild);
+    }
+  }
+  /* actualiza el valor de kmInput cuando el event lisener del contructor recive un input luego actualiza el valor de kmValueInput */
+  handleKmInputChange() {
+    this.kmValue = this.kmInput.value;
+    this.kmValueInput.value = this.kmValue;
+  }
+  /* actualiza el valor de kmValueInput cuando el event lisener del contructor recive un input luego actualiza el valor de kmInput */
+  handleKmValueInputChange() {
+    this.kmValue = this.kmValueInput.value;
+    this.kmInput.value = this.kmValue;
+  }
+}
 export default FormCreate;
--- components/fetchApi.js
+++ components/fetchApi.js
@@ -1,87 +1,95 @@
-import apiKey from './apiKey.js'
+import apiKey from "./apiKey.js";
 
 class FetchFromApi {
-    /* Guarda toda la información de los fetch */
-    constructor() {
-        this.brand = []
-        this.models = []
-        this.emisions = ""
-    }
-    /* Hace fetch para guardar todos las marcas en brand */
-    async getBrand() {
-        try {
-          let response = await fetch("https://www.carboninterface.com/api/v1/vehicle_makes", {
-            method: "GET",
-            headers: {
-              Authorization: apiKey,
-              "Content-Type": "application/json",
-            },
-          });
-          let data = await response.json();
-    
-          // Guardar los nombres y los IDs de las marcas de vehículos en this.brand
-          data.forEach((item) => {
-            const id = item.data.id;
-            const name = item.data.attributes.name;
-            this.brand.push({ id, name });
-          });
-          return this.brand;
-        } catch (err) {
-          console.error(err);
-          return null;
+  /* Guarda toda la información de los fetch */
+  constructor() {
+    this.brand = [];
+    this.models = [];
+    this.emisions = "";
+  }
+  /* Hace fetch para guardar todos las marcas en brand */
+  async getBrand() {
+    try {
+      let response = await fetch(
+        "https://www.carboninterface.com/api/v1/vehicle_makes",
+        {
+          method: "GET",
+          headers: {
+            Authorization: apiKey,
+            "Content-Type": "application/json",
+          },
         }
-      }
-    /* Hace fetch para guardar todos los modelos */
-    async getModel(id) {
-        try {
-            let response = await fetch(`https://www.carboninterface.com/api/v1/vehicle_makes/${id}/vehicle_models`, {
-              method: "GET",
-              headers: {
-                Authorization: apiKey,
-                "Content-Type": "application/json",
-              },
-            });
-            let data = await response.json();
-      
-            // Guardar los nombres y los IDs de las marcas de vehículos en this.brand
-            data.forEach((item) => {
-              const id = item.data.id;
-              const name = item.data.attributes.name;
-              const year = item.data.attributes.year;
-              this.models.push({id, name, year});
-            });
-      
-            console.log(this.models);
-            return this.models;
-          } catch (err) {
-            console.error(err);
-            return null;
-          }
-        }
-    /* Hace fetch para consegir la cantidad de emisiones */
-    async getEstimates(id,km) {
-        try {
-            let response = await fetch("https://www.carboninterface.com/api/v1/estimates", {
-            method: "POST",
-            headers: {
-                Authorization: apiKey,
-                "Content-Type": "application/json",
-            },
-            body: JSON.stringify({
-                type: "vehicle",
-                distance_unit: "km",
-                distance_value: km,
-                vehicle_model_id: id,
-            }),
-            });
-            let data = await response.json();
-            this.emisions = data.data.attributes.carbon_kg;
-            return this.emisions;
-        } catch (err) {
-            console.error(err);
-            return null;
-        }
-        }
-    }
+      );
+      let data = await response.json();
 
-export default FetchFromApi
+      // Guardar los nombres y los IDs de las marcas de vehículos en this.brand
+      data.forEach((item) => {
+        const id = item.data.id;
+        const name = item.data.attributes.name;
+        this.brand.push({ id, name });
+      });
+      return this.brand;
+    } catch (err) {
+      console.error(err);
+      return null;
+    }
+  }
+  /* Hace fetch para guardar todos los modelos */
+  async getModel(id) {
+    try {
+      let response = await fetch(
+        `https://www.carboninterface.com/api/v1/vehicle_makes/${id}/vehicle_models`,
+        {
+          method: "GET",
+          headers: {
+            Authorization: apiKey,
+            "Content-Type": "application/json",
+          },
+        }
+      );
+      let data = await response.json();
+
+      // Guardar los nombres y los IDs de las marcas de vehículos en this.brand
+      data.forEach((item) => {
+        const id = item.data.id;
+        const name = item.data.attributes.name;
+        const year = item.data.attributes.year;
+        this.models.push({ id, name, year });
+      });
+
+      console.log(this.models);
+      return this.models;
+    } catch (err) {
+      console.error(err);
+      return null;
+    }
+  }
+  /* Hace fetch para consegir la cantidad de emisiones */
+  async getEstimates(id, km) {
+    try {
+      let response = await fetch(
+        "https://www.carboninterface.com/api/v1/estimates",
+        {
+          method: "POST",
+          headers: {
+            Authorization: apiKey,
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({
+            type: "vehicle",
+            distance_unit: "km",
+            distance_value: km,
+            vehicle_model_id: id,
+          }),
+        }
+      );
+      let data = await response.json();
+      this.emisions = data.data.attributes.carbon_kg;
+      return this.emisions;
+    } catch (err) {
+      console.error(err);
+      return null;
+    }
+  }
+}
+export default FetchFromApi;
